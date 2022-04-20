97ca2d2296bb2d021955944d36336bf38ef5ebf7
Sangram_29 <sangramscience29@gmail.com>
2022-04-20 21:14:42 +0530

    New Web Project

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "LAVENDAR KF102 Drone",
+    productNumber: "35378",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "96996",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "DJI Air 25",
+    productNumber: "81475",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lozenge Drone",
+    productNumber: "000482",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
